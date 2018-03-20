ed5d928745f3e0487430d8dfd203c1f0fd5e8cb2
Hinaser <rindo.hinase@gmail.com>
2018-03-20 18:37:01 +0900

    [WIP] Provider list can be added/edited/removed

--- src/popup/index.js
+++ src/popup/index.js
@@ -15,13 +15,14 @@ function main(){
     getSearchProviders().then(providers => {
       let template = document.querySelector("#searchProvider-template");
       let table = document.querySelector(".provider-list");
-    
-      if(!Array.isArray(providers) || providers.length < 1){
-        return;
-      }
       
       table.querySelectorAll(".row").forEach(el => el.remove());
-    
+  
+      if(!Array.isArray(providers) || providers.length < 1){
+        resetHeight();
+        return;
+      }
+  
       let providerElements = providers.map(p => {
         let nameElement = template.content.querySelector(".name");
         let baseUrlElement = template.content.querySelector(".baseurl");
@@ -33,8 +34,96 @@ function main(){
         let clone = document.importNode(template.content, true);
         table.appendChild(clone);
       });
+      resetHeight();
+    
+      let editBtns = document.querySelectorAll(".provider-list .modify");
+      let editBtnAction = evt => {
+        if(evt.target.tagName.toLowerCase() !== "i") return;
+  
+        let row = evt.target.parentElement.parentElement;
+        let nameElement = row.querySelector(".name");
+        let baseurlElement = row.querySelector(".baseurl");
+        let name = nameElement.textContent;
+        let baseurl = baseurlElement.textContent;
+        
+        let onInput = e => {
+          if(e.key === "Enter"){
+            let row = evt.target.parentElement.parentElement;
+            let nameElement = row.querySelector(".name");
+            let baseurlElement = row.querySelector(".baseurl");
+            let name = nameElement.textContent;
+            let baseurl = baseurlElement.textContent;
+            
+            setSearchProviders({name, baseurl});
+            refreshProviders();
+          }
+        };
+  
+        let nameInput = document.createElement("input");
+        nameInput.type = "text";
+        nameInput.value = name;
+        nameInput.className = "name";
+        nameInput.addEventListener("click", onInput);
+  
+        let baseurlInput = document.createElement("input");
+        baseurlInput.type = "text";
+        baseurlInput.value = baseurl;
+        baseurlInput.className = "baseurl";
+        baseurlInput.addEventListener("click", onInput);
+  
+        let addProviderBtn = document.querySelector(".add-provider .add");
+        addProviderBtn.style.display = "none";
+  
+        let modifyBtns = document.querySelectorAll(".provider-list .modify");
+        modifyBtns.forEach(btn => {
+          btn.style.display = "none";
+        });
+        evt.target.style.display = "inline-block";
+  
+        let removeBtns = document.querySelectorAll(".provider-list .remove");
+        removeBtns.forEach(btn => {
+          btn.style.display = "none";
+        });
+        
+        let cancelBtn = row.querySelector(".cancel");
+        cancelBtn.style.display = "inline-block";
+  
+        row.replaceChild(nameInput, nameElement);
+        row.replaceChild(baseurlInput, baseurlElement);
+        row.classList.add("editing");
+      };
       
-      document.querySelector("html").style.height = document.body.offsetHeight;
+      editBtns.forEach(editBtn => {
+        editBtn.addEventListener("click", editBtnAction, true);
+      });
+  
+      let removeBtns = document.querySelectorAll(".provider-list .remove");
+      removeBtns.forEach(removeBtn => {
+        removeBtn.addEventListener("click", evt => {
+          let row = evt.target.parentElement.parentElement;
+          let nameElement = row.querySelector(".name");
+          let name = nameElement.textContent;
+          setSearchProviders({name, remove: true}).then(()=>{
+            refreshProviders();
+          });
+        });
+      });
+      
+      let cancelBtns = document.querySelectorAll(".provider-list .cancel");
+      cancelBtns.forEach(cancelBtn => {
+        cancelBtn.addEventListener("click", evt => {
+          refreshProviders();
+        });
+      });
+      
+      let addProviderBtn = document.querySelector(".add-provider .add");
+      addProviderBtn.style.display = "inline-block";
+      
+      let editing = document.querySelector(".row.editing");
+      if(editing){
+        editing.classList.remove("editing");
+      }
+  
     });
   };
   
@@ -61,38 +150,50 @@ function main(){
     checkBtn.style.display = "inline-block";
     cancelBtn.style.display = "inline-block";
   
-    checkBtn.addEventListener("click", evt => {
-      let row = document.querySelector(".provider-list .adding");
-      let name = row.querySelector(".name input").value;
-      let baseurl = row.querySelector(".baseurl input").value;
-      
-      if(!name || !baseurl){
-        alert("Invalid");
-        return;
-      }
+  });
   
-      row.remove();
-  
-      addProviderBtn.style.display = "inline-block";
-      checkBtn.style.display = "none";
-      cancelBtn.style.display = "none";
-  
-      setSearchProviders({name, baseurl}).then(()=>{
-        refreshProviders()
-      });
-    });
-  
-    cancelBtn.addEventListener("click", evt => {
-      let row = document.querySelector(".provider-list .adding");
-      row.remove();
-      
-      addProviderBtn.style.display = "inline-block";
-      checkBtn.style.display = "none";
-      cancelBtn.style.display = "none";
-  
-      refreshProviders();
+  let checkBtn = document.querySelector(".add-provider .check");
+  checkBtn.addEventListener("click", evt => {
+    let row = document.querySelector(".provider-list .adding");
+    let name = row.querySelector(".name input").value;
+    let baseurl = row.querySelector(".baseurl input").value;
+    let cancelBtn = document.querySelector(".add-provider .cancel");
+    
+    if(!name || !baseurl){
+      alert("Invalid");
+      return;
+    }
+    
+    row.remove();
+    
+    addProviderBtn.style.display = "inline-block";
+    checkBtn.style.display = "none";
+    cancelBtn.style.display = "none";
+    
+    setSearchProviders({name, baseurl}).then(()=>{
+      refreshProviders()
     });
   });
+  
+  let cancelBtn = document.querySelector(".add-provider .cancel");
+  cancelBtn.addEventListener("click", evt => {
+    let row = document.querySelector(".provider-list .adding");
+    let checkBtn = document.querySelector(".add-provider .check");
+    row.remove();
+    
+    addProviderBtn.style.display = "inline-block";
+    checkBtn.style.display = "none";
+    cancelBtn.style.display = "none";
+    
+    refreshProviders();
+  });
+}
+
+function resetHeight(){
+  let mainDiv = document.querySelector("#main");
+  let contentHeight = mainDiv.offsetHeight + "px";
+  document.body.style.height = contentHeight;
+  document.querySelector("html").style.height = contentHeight;
 }
 
 function requestIframe(){
@@ -113,17 +214,22 @@ function getSearchProviders(){
   });
 }
 
-function setSearchProviders({name, baseurl}){
+function setSearchProviders({name, baseurl, remove}){
   return new Promise((resolve, reject) => {
     getSearchProviders().then(providers => {
       if(!Array.isArray(providers)) providers = [];
       
-      let index = providers.findIndex(p => p.name === name);
-      if(index > -1){
-        providers[index].baseurl = baseurl;
+      if(remove === true){
+        providers = providers.filter(p => p.name !== name);
       }
       else{
-        providers.push({name, baseurl});
+        let index = providers.findIndex(p => p.name === name);
+        if(index > -1){
+          providers[index].baseurl = baseurl;
+        }
+        else{
+          providers.push({name, baseurl});
+        }
       }
       
       chrome.storage.sync.set({providers}, ()=> {
